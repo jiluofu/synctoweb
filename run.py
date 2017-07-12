a52ebb1f924f1d91b0569402f9a4f309ffd1f006
zhuxu <zhuxu@baidu.com>
2017-07-12 15:42:18 +0800

    =更新

--- run.py
+++ run.py
@@ -82,9 +82,9 @@ for i in range(0, len(qsj)):
     qsj_folder_arr.append(qsj_folder)
 
 
-# lofter.pub(file_parent_path, folder)
-# zhihu.pub(file_parent_path, folder)
-# weibo.pub(file_parent_path, folder)
+lofter.pub(file_parent_path, folder)
+zhihu.pub(file_parent_path, folder)
+weibo.pub(file_parent_path, folder)
 mpwx.pub(file_parent_path, folder, qsj_folder_arr, url)
 
 init.clean_tmp(file_parent_path + os.sep + 'tmp')
