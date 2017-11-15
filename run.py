db2865aaa705000ee2629b4bc21524e32fc33379
zhuxu <zhuxu@baidu.com>
2017-11-16 07:32:25 +0800

    =update

--- run.py
+++ run.py
@@ -85,7 +85,7 @@ for i in range(0, len(qsj)):
 lofter.pub(file_parent_path, folder)
 zhihu.pub(file_parent_path, folder)
 weibo.pub(file_parent_path, folder)
-# mpwx.pub(file_parent_path, folder, qsj_folder_arr, url)
+mpwx.pub(file_parent_path, folder, qsj_folder_arr, url)
 
 init.clean_tmp(file_parent_path + os.sep + 'tmp')
 
