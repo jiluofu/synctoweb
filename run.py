e76fb9ce59894af54144bd612d7d2cf694430871
zhuxu <zhuxu@baidu.com>
2019-11-16 07:46:32 +0800

    =update

--- run.py
+++ run.py
@@ -86,9 +86,9 @@ for i in range(0, len(qsj)):
 
 mpwx.pub(file_parent_path, folder, qsj_folder_arr, url)
 lofter.pub(file_parent_path, folder)
-douban.pub(file_parent_path, folder)
 zhihu.pub(file_parent_path, folder)
 weibo.pub(file_parent_path, folder)
+douban.pub(file_parent_path, folder)
 
 
 
